614d82c47456dcef64ad7ac61af5ee38ac6caaa7
Raidel <15664418+raidel-a@users.noreply.github.com>
2024-11-27 17:03:40 -0500

    Update .gitignore and refactor secrets handling; add support for multiple WiFi networks

---
+++ Hardware/src/secrets.h
@@ -0,0 +1,16 @@
+   // secrets.h
+   #ifndef SECRETS_H
+   #define SECRETS_H
+
+   // Network 1 - WPA2 Enterprise (eduroam)
+   extern const char* ssid1;
+   extern const char* password1;
+   extern const char* identity1;
+   extern const char* mqtt_server1;
+
+   // Network 2 - Regular WiFi
+   extern const char* ssid2;
+   extern const char* password2;
+   extern const char* mqtt_server2;
+
+   #endif
