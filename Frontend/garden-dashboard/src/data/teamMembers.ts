399f12a93be2332cf2df4a52ad48d011834a46c0
Raidel <15664418+raidel-a@users.noreply.github.com>
2024-12-06 14:21:24 -0500

    Update team members' information and bios in teamMembers.ts

--- Frontend/garden-dashboard/src/data/teamMembers.ts
+++ Frontend/garden-dashboard/src/data/teamMembers.ts
@@ -11,98 +11,106 @@ export interface TeamMember {
   export const teamMembers: TeamMember[] = [
     {
       id: 1,
-      name: "Jane Doe",
-      role: "Lead Developer",
-      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      name: "Raidel Almeida",
+      role: "Team Co-Lead",
+      bio: "Majoring in Computer Science, my interest vary from Hardware/Robotics, Web Developent to IOS and MacOS development.",
       imageUrl: "/textures/team/member1.jpg",
-      link: "https://www.google.com"
+      link: "https://raidel.dev"
     },
     {
       id: 2,
-      name: "John Smith",
-      role: "UI/UX Designer",
-      bio: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      name: "Anncarolyne Power",
+      role: "Team Co-Lead",
+      bio: "Hi my name is Carol and I am a CS major interested in becoming a SWE or PM. I also really love being creative and designing mobile apps.",
       imageUrl: "/textures/team/member2.jpg",
-      link: "https://www.google.com"
+      link: "https://linkedin.com/in/anncarolyne-power"
     },
     {
       id: 3,
-      name: "Alice Johnson",
-      role: "Backend Engineer",
-      bio: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      name: "Mia Urra",
+      role: "UI/UX Designer",
+      bio: "Hello my name is Mia Urra and I am a Sophomore Information Systems major. I am interested in learning new things and being involved in school!",
       imageUrl: "/textures/team/member3.jpg",
-      link: "https://www.google.com"
+      link: "https://linkedin.com/in/mia-urra-4a74092b9"
     },
     {
       id: 4,
-      name: "Bob Wilson",
+      name: "Tyler Coy",
       role: "DevOps Engineer",
-      bio: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      bio: "Hello, name is Tyler Coy and I am a junior at Fiu majoring in computer engineering. I am interested in robotics and software engineering.",
       imageUrl: "/textures/team/member4.jpg",
-      link: "https://www.google.com"
+      link: ""
     },
     {
       id: 5,
-      name: "Eve Doe",
-      role: "Developer",
-      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      name: "Ronaldo Carrazco",
+      role: "Embedded Engineer",
+      bio: "Hello, my name is Ronaldo Carrazco and I am a senior at FIU majoring in Computer Science with an interested in robotics and computer vision.",
       imageUrl: "/textures/team/member5.jpg",
-      link: "https://www.google.com"
+      link: ""
     },
     {
       id: 6,
-      name: "Ethel Cain",
-      role: "Developer",
-      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      name: "Tyler Hebron",
+      role: "Embedded Engineer",
+      bio: "Hello! My name is Tyler Hebron. I am a junior at FIU studying Computer Engineering. My interests include everything from software development to embedded programming!",
       imageUrl: "/textures/team/member6.jpg",
-      link: "https://www.google.com"
+      link: ""
     },
     {
       id: 7,
-      name: "Joe Talbot",
-      role: "Developer",
-      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      name: "Huu Thang Ly",
+      role: "Embedded Engineer",
+      bio: "Hi, my name is Huu Thang Ly (Leo) and I am currently a junior at FIU majoring in Computer Engineering. I'm interested in embedded systems, robotics, AI and software engineering.",
       imageUrl: "/textures/team/member7.jpg",
-      link: "https://www.google.com"
+      link: "https://linkedin.com/in/huu-thang-ly/"
     },
     {
       id: 8,
-      name: "John John John",
-      role: "Developer",
-      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      name: "Steven Rodriguez",
+      role: "Embedded Engineer",
+      bio: "Studying Mechanical Engineering at FIU with an interest in embedded systems and software engineering.",
       imageUrl: "/textures/team/member8.jpg",
-      link: "https://www.google.com"
+      link: "https://steven-rodriguezz.carrd.co"
     },
     {
       id: 9,
-      name: "Micasa",
+      name: "Pooja Lad",
       role: "Developer",
-      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      bio: "Hi! My name is Pooja and I’m a CS major with a bio minor. I’m super interested in the intersection between science and tech",
       imageUrl: "/textures/team/member9.jpg",
-      link: "https://www.google.com"
+      link: ""
     },
     {
       id: 10,
-      name: "Kyle Dixon",
+      name: "Arantza Mendoza",
       role: "Developer",
-      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      bio: "Hi! My name is Arantza Mendoza. I am a Junior CS major. I’m interested in finding solutions to problems through code. I am a constant and fast learner. You’ll always see me learning something new.",
       imageUrl: "/textures/team/member10.jpg",
-      link: "https://www.google.com"
+      link: "https://linkedin.com/in/arantza-mendoza"
     },
     {
         id: 11,
-        name: "Lora Hightower",
+        name: "Bielky Ruiz",
         role: "Developer",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        bio: "Hello my name is Bielky Ruiz and I am a Senior at FIU majoring in Computer Science. I am interested in Data Analytics and Artificial Intelligence.",
         imageUrl: "/textures/team/member11.jpg",
-        link: "https://www.google.com"
+        link: ""
     },
     {
         id: 12,
-        name: "Ada Lovelace",
-        role: "Developer",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        name: "Carlos Amasifuen",
+        role: "Developer & Cybersecurity Consultant",
+        bio: "Hi, I'm Carlos Amasifuen, a graduate student at FIU majoring in Cybersecurity. With a background in web development and UI/UX design, I'm now focusing on advancing my skills in the cybersecurity field.",
         imageUrl: "/textures/team/member12.jpg",
-        link: "https://www.google.com"
-    }
+        link: "https://linkedin.com/in/camasifuen6/"
+    },  
+    {
+        id: 13,
+        name: "Jin Carballosa",
+        role: "Developer",
+        bio: "Interested in leveraging the web to help others.",
+        imageUrl: "/textures/team/member13.jpg",
+        link: ""
+     }
   ];
